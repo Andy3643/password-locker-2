1509870685c530ae09dede07092fd7ccb05d8568
@Andy <joshua.mulonga@student.moringaschool.com>
2022-05-05 18:53:01 +0300

    add class method to dispay user and delete user instance

--- credentails.py
+++ credentails.py
@@ -1,3 +1,7 @@
+from requests import delete
+import pyperclip
+
+
 class User:
     #list to store users
     user_list = []
@@ -11,3 +15,13 @@ class User:
         method to save created user instance
         """
         User.user_list.append(self)
+
+    @classmethod
+    def display_user(cls):
+        return cls.user_list
+
+    def delete_user (self):
+        """
+        method to delete user
+        """
+        User.user_list.remove(self)
