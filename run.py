4755d3dba00f00190e6f141f05b7b5ae43e6d297
@Andy <joshua.mulonga@student.moringaschool.com>
2022-05-06 08:23:52 +0300

    create and save new credentials for the user

--- run.py
+++ run.py
@@ -20,3 +20,14 @@ def login_user (username,password):
     #allow user to login
     check_user = Credentials.user_verification(username,password)
     return check_user
+
+def create_new_credential(account,username,password):
+    
+    #create new credentials 
+    new_credentials= Credentials(account,username,password)
+    return new_credentials
+    
+def save_credentials(credentials):
+
+    #save credentials to the credentials list
+    credentials.save_datails()
